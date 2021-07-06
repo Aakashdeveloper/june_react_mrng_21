bd64d41d1760f769d4f0ced87b25f32b294ebd42
aakashdeveloper <ahanda205@gmail.com>
2021-07-06 07:51:01 +0530

    apiCalling

--- thirdapp/src/Component/Home/QuickSearch.js
+++ thirdapp/src/Component/Home/QuickSearch.js
@@ -1,11 +1,31 @@
 import React,{ Component }from 'react';
+import QuickDisplay from './QuickDisplay';
 
+const url = "https://developerfunnel.herokuapp.com/booking"
 class QuickSearch extends Component {
+    constructor(){
+        super()
+
+        this.state={
+            tripType:''
+        }
+    }
+
     render(){
         return(
-            <h1>QuickSearch</h1>
+            <QuickDisplay tripdata={this.state.tripType}/>
         )
     }
+
+    //api call 
+    componentDidMount(){
+        fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            this.setState({tripType:data})
+        })
+    }
+
 }
 
 export default QuickSearch
--- thirdapp/src/Component/Home/Search.js
+++ thirdapp/src/Component/Home/Search.js
@@ -1,16 +1,55 @@
 import React,{ Component }from 'react';
 import './Search.css';
 
+const CityUrl = "https://developerfunnel.herokuapp.com/location";
+const hotelUrl = "https://developerfunnel.herokuapp.com/hotels?city="
+
 class Search extends Component {
     constructor(props){
         super(props)
-
+        console.log(">>>>>inside constructor ")
         this.state={
             location:'',
             hotels:''
         }
     }
+
+    renderCity = (data) => {
+        if(data){
+            return data.map((item) => {
+                return(
+                    <option value={item._id}>
+                        {item.city_name}
+                    </option>
+                )
+            })
+        }
+    }
+
+    renderHotel = (data) => {
+        if(data){
+            return data.map((item) => {
+                return(
+                    <option value={item._id}>
+                        {item.name} | {item.locality}
+                    </option>
+                )
+            })
+        }
+    }
+    handleCity = (event) => {
+        console.log(event.target.value)
+        const cityId = event.target.value;
+        fetch(`${hotelUrl}${cityId}`,{method:'GET'})
+        .then((res) =>  res.json())
+        .then((data) => {
+            console.log(data)
+            this.setState({hotels:data})
+        })
+    }
+
     render(){
+        console.log(">>>>>inside render ",this.state.location)
         return(
             <div className="imageContainer">
                 <div id="logo">
@@ -20,15 +59,36 @@ class Search extends Component {
                     Plan Trip With Us
                 </div>
                 <div className="locationSelector">
-                    <select class="locationDropDown">
+                    <select class="locationDropDown" onChange={this.handleCity}>
                         <option>-------SELECT CITY-----------</option>
+                        {this.renderCity(this.state.location)}
                     </select>
                     <select class="hotelDropDown">
                         <option>-------SELECT HOTEL-----------</option>
+                        {this.renderHotel(this.state.hotels)}
                     </select>
                 </div>
             </div>
         )
+    }
+
+    //on page load call api 
+    componentDidMount(){
+        console.log(">>>>>inside component Did Mount ")
+        // we get the data and update state
+        fetch(CityUrl, {method:'GET'})
+        // return the promise here
+        .then((res) => res.json())
+        // get the data
+        .then((data) => {
+            // console.log(data)
+            //setting data in state
+            this.setState({location:data})
+        })
+        // error handling
+        .catch((err) => {
+            console.log(err)
+        })
     }
 }
 
